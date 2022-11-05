d0060c178578df1c0ba9632ee2a631a656c20d30
tarekmn <nytarek@gmail.com>
2022-11-05 10:52:56 -0500

    relationships

--- utils/seed.js
+++ utils/seed.js
@@ -16,36 +16,30 @@ connection.once("open", async () => {
   await Reaction.deleteMany({});
 
   // Insert users
-  await User.insertMany(
-    [
-      {
-        username: "Tarek",
-        email: "test@gmail.com",
-      },
-      {
-        username: "Ben",
-        email: "test2@gmail.com",
-      },
-    ],
-    (insertError) =>
-      insertError ? handleError(insertError) : console.log("Inserted")
-  );
 
-  await Thought.insertMany(
-    [
-      {
-        thoughtText: "Hello these are my thoughts",
-        username: "6365783bd61b786979c6705f",
-      },
-      {
-        thoughtText: "This is the second thought",
-        username: "6365783bd61b786979c67060",
-      },
-    ],
-    (insertError) =>
-      insertError ? handleError(insertError) : console.log("Inserted")
-  );
+  const user1 = await User.create({
+    username: "Tarek",
 
+    email: "test@gmail.com",
+  });
+
+  const user2 = await User.create({
+    username: "Ben",
+    email: "test2@gmail.com",
+  });
+
+  // Insert thought
+  const thought1 = await Thought.create({
+    thoughtText: "Hello these are my thoughts",
+    username: user1.username,
+  });
+
+  const thought2 = await Thought.create({
+    thoughtText: "This is the second thought",
+    username: user2.username,
+  });
+
+  // Insert reaction
   await Reaction.insertMany(
     [
       {
@@ -64,5 +58,4 @@ connection.once("open", async () => {
   // Log out the seed data to indicate what should appear in the database
   console.table(User);
   console.info("Seeding complete!");
-  // process.exit(0);
 });
